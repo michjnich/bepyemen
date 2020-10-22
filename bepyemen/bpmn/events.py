eca6556df86d81acd478894cea59d29928306437
Michael J Nicholson <mjnicholson@outlook.com>
2020-10-23 00:06:45 +0200

    Added empty BPMN events

---
+++ bepyemen/bpmn/events.py
@@ -0,0 +1,17 @@
+from bepyemen.core.shapes import Circle
+
+
+class StartEvent(Circle):
+    pass
+
+
+class EndEvent(Circle):
+    pass
+
+
+class Timer(Circle):
+    pass
+
+
+class IntermediateEvent(Circle):
+    pass
